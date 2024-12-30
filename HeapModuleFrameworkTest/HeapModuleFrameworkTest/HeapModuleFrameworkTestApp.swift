d9e00dafe9870af85170f8530a1d3da10935c318
Abhishek Shukla <abhishekshuklaa666@gmail.com>
2024-12-30 13:03:36 +0530

    Added new folder
    Heap Module folder restructuring

---
+++ HeapModuleFrameworkTest/HeapModuleFrameworkTest/HeapModuleFrameworkTestApp.swift
@@ -0,0 +1,18 @@
+//
+//  HeapModuleFrameworkTestApp.swift
+//  HeapModuleFrameworkTest
+//
+//  Created by Abhishek Shukla on 30/12/24.
+//
+
+import SwiftUI
+import Swift-Essentials
+
+@main
+struct HeapModuleFrameworkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeapModuleFrameworkTest/HeapModuleFrameworkTestTests/HeapModuleFrameworkTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeapModuleFrameworkTestTests.swift
+//  HeapModuleFrameworkTestTests
+//
+//  Created by Abhishek Shukla on 30/12/24.
+//
+
+import Testing
+@testable import HeapModuleFrameworkTest
+
+struct HeapModuleFrameworkTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
